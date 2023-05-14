b2bfb5259bef007451e4a534b18534a78595b345
Divante Parness <divante111@gmail.com>
2023-05-14 17:49:58 -0500

    finished putting animation into lists

--- main.py
+++ main.py
@@ -11,13 +11,114 @@ clock = pygame.time.Clock()
 bg = pygame.image.load('bg.png')
 
 bg_width = bg.get_width()
-bg_height = bg.get_height()
+bg_height = bg.get_height() 
 tiles = math.ceil(1000 / bg_width)
-
+black = (0,0,0)
 
 scroll = 0
-standing = pygame.image.load('standing.jpg')
+sprite_sheet_image = pygame.image.load('knights.png').convert_alpha()
 
+
+
+
+
+class SpriteSheet():
+    def __init__(self, image):
+        self.sheet = image
+
+    def get_image(self, frame, width, height, scale, colour):
+        image = pygame.Surface((width, height)).convert_alpha()
+        image.blit(self.sheet, (0,0), ((frame * width), 0, width, height))
+        image = pygame.transform.scale(image, (width, height))
+        image.set_colorkey(colour)
+
+        return image
+    
+    def get_image_2(self, frame, width, height, scale, colour):
+        image = pygame.Surface((width, height)).convert_alpha()
+        image.blit(self.sheet, (0,0), ((frame * width), 110, width , 120))
+        image = pygame.transform.scale(image, (width, height))
+        image.set_colorkey(colour)
+
+        return image
+    
+    def get_image_3(self, frame, width, height, scale, colour):
+        image = pygame.Surface((width, height)).convert_alpha()
+        image.blit(self.sheet, (0,0), ((frame * width), 220, width , 120))
+        image = pygame.transform.scale(image, (width, height))
+        image.set_colorkey(colour)
+
+        return image
+    
+    def get_image_4(self, frame, width, height, scale, colour):
+        image = pygame.Surface((width, height)).convert_alpha()
+        image.blit(self.sheet, (0,0), ((frame * width), 330, width , 120))
+        image = pygame.transform.scale(image, (width, height))
+        image.set_colorkey(colour)
+
+        return image
+    
+
+
+
+image_count = 0  
+
+
+king_down = []
+king_left = []
+king_right = []
+king_up = []
+
+
+knight_down = []
+knight_left = []
+knight_right = []
+knight_up = []
+
+
+knight_black_down = []
+knight_black_left = []
+knight_black_right = []
+knight_black_up = []
+
+
+knight_green_down = []
+knight_green_left = []
+knight_green_right = []
+knight_green_up = []
+
+
+down_animations_list = [king_down, knight_down, knight_black_down, knight_green_down]
+left_animations_list = [king_left, knight_left, knight_black_left, knight_green_left]
+right_animations_list = [king_right, knight_right, knight_black_right, knight_green_right]
+up_animations_list = [king_up, knight_up, knight_black_up, knight_green_up]
+
+for i in down_animations_list:
+    for j in range(3):
+        i.append(SpriteSheet(sprite_sheet_image).get_image(image_count, 79, 110, 3, black))
+        image_count+=1
+image_count = 0  
+
+for i in left_animations_list:
+    for j in range(3):
+        i.append(SpriteSheet(sprite_sheet_image).get_image_2(image_count, 79, 220, 3, black))
+        image_count+=1
+image_count = 0  
+
+for i in right_animations_list:
+    for j in range(3):
+        i.append(SpriteSheet(sprite_sheet_image).get_image_3(image_count, 79, 220, 3, black))
+        image_count+=1
+image_count = 0  
+
+for i in up_animations_list:
+    for j in range(3):
+        i.append(SpriteSheet(sprite_sheet_image).get_image_4(image_count, 79, 330, 3, black))
+        image_count+=1
+image_count = 0  
+  
+
+    
 class player(object):
     def __init__(self, x, y, width, height):
         self.x = x
@@ -29,7 +130,8 @@ class player(object):
         self.vel = 20
 
     def draw(self, win):
-        win.blit(standing, (self.x, self.y))
+        win.blit(king_up[0], (self.x, self.y))
+
 
 
 character = player(20, 100, 64, 64)
